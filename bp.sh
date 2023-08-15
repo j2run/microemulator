15ff722cf1ccf49c2d96d28a58b3c0eb49062e30
wawahuy <kakahuy99@gmail.com>
2023-08-15 21:25:25 +0700

    update build

--- bp.sh
+++ bp.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+c=$(id | grep 'yuh')
+if [ -z "$c" ]; then
+    echo 'Run: su yuh'
+    exit
+fi
+
 mvn -Dmaven.test.skip=true
-java -jar -Djava.library.path=microemulator/target microemulator/target/microemulator-2.0.4.jar -jar test-j2me.jar
+java -jar -Djava.library.path=microemulator/target microemulator/target/microemulator-2.0.4.jar -jar test-j2me.jar --resizableDevice 240 320
